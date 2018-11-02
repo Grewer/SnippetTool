99da4453ba38034a607749bfeccd026d67fc511d
Grewer <grewer@grewer.cn>
2018-11-02 11:10:23 +0800

    feat(location): 编辑问题待解决

--- main.ts
+++ main.ts
@@ -1,27 +1,16 @@
-import { enableLiveReload } from 'electron-compile';
+
 const {app, BrowserWindow} = require('electron')
-
-let appRoot = __dirname
-
-require('electron-compile').init(appRoot, require.resolve('./main'));
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
-
-const isDevMode = process.execPath.match(/[\\/]electron/);
-
-if (isDevMode) {
-  enableLiveReload({ strategy: 'react-hmr' });
-}
 
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('src/index.html')
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
@@ -39,7 +28,6 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
-
 
 
 // enableLiveReload({strategy: 'react-hmr'});
