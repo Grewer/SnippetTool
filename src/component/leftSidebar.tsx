4f8d3b59b256d7c82dbde2dbe91d9c9843abd416
zhangyu <grewer@grewer.cn>
2018-11-06 22:44:54 +0800

    feat(location): input css (radium 对于 hover focus仍然有 bug ,后面估计会换)

--- src/component/leftSidebar.tsx
+++ src/component/leftSidebar.tsx
@@ -4,12 +4,26 @@ import Radium from 'radium';
 const styles = {
   main: {
     background: 'white',
-    borderRight:'1px solid #999',
+    borderRight: '1px solid #999',
     borderLeft: 0,
     borderTopLeftRadius: 4,
     borderBottomLeftRadius: 4,
     padding: '1em',
     flexBasis: "30%",
+  },
+  input: {
+    backgroundColor: "white",
+    cursor: 'text',
+    padding: '2px',
+    border: '1px solid #ddd',
+    textIndent: '5px',
+    outline: 0,
+    ':hover': {
+      borderColor: '#a3af2a'
+    },
+    ':focus': {
+      borderColor: 'rgba(20,105,144,0.5)'
+    }
   }
 };
 
@@ -17,7 +31,7 @@ const styles = {
 class LeftSidebar extends React.PureComponent {
   public render() {
     return (<div style={styles.main}>
-      <div><input type="text" placeholder="请输入关键词"/><i>icon</i></div>
+      <div><input type="text" style={styles.input} placeholder="请输入关键词"/><i>i</i></div>
       <div>
         <ul>
           <li>test1</li>
