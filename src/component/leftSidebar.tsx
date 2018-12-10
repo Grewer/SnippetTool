d89123bdc86d92e2340f2f507ee2bec8d4ccc0c5
zhangyu <grewer@grewer.cn>
2018-12-10 22:43:13 +0800

    feat(location): list li 列表样式优化

--- src/component/leftSidebar.tsx
+++ src/component/leftSidebar.tsx
@@ -4,15 +4,13 @@ import * as React from 'react';
 class LeftSidebar extends React.PureComponent {
   public render() {
     return (<div className="leftBar">
-      <div><input className="search" type="text" placeholder="请输入关键词"/><i>i</i></div>
-      <div>
-        <ul>
-          <li>test1</li>
-          <li>test1</li>
-          <li>test1</li>
-          <li>test1</li>
-        </ul>
-      </div>
+      <div><input className="search" type="text" placeholder="请输入关键词"/></div>
+      <ul className="list">
+        <li> 测试长度XXXXXXXXXXXXXX</li>
+        <li className="active">test1</li>
+        <li>test1</li>
+        <li>test1</li>
+      </ul>
     </div>);
   }
 }
