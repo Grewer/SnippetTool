018660b5dfe5bdf7e946e09fad38c97bdd449e6f
zhangyu <grewer@grewer.cn>
2020-02-13 00:01:14 +0800

    feat(module): 防抖优化

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import { PlainTextEdit, Text, View } from '@nodegui/react-nodegui'
 import showdown from 'showdown'
 import markDownCSS from '~/style/MarkDownCSS'
@@ -8,22 +8,26 @@ function Content() {
   const TextEditRef: React.MutableRefObject<any> = useRef()
   const [preView, setPreView] = useState('')
 
+  const textChangeHandle = useCallback(() => {
+    const text2 = TextEditRef.current.toPlainText()
+    const converter = new showdown.Converter({
+      strikethrough: true,
+      tables: true,
+      tasklists: true,
+      requireSpaceBeforeHeadingText: true,
+      emoji: true,
+      omitExtraWLInCodeBlocks: true,
+    })
+    const html = converter.makeHtml(text2)
+    setPreView(html)
+  }, [TextEditRef])
+
   console.log('render Content component')
   return (<View id="content">
     <PlainTextEdit ref={TextEditRef}
                    on={{
-                     textChanged: (text?) => {
-                       const text2 = TextEditRef.current.toPlainText()
-                       const converter = new showdown.Converter({
-                         strikethrough: true,
-                         tables: true,
-                         tasklists: true,
-                         requireSpaceBeforeHeadingText: true,
-                         emoji: true,
-                         omitExtraWLInCodeBlocks: true,
-                       })
-                       const html = converter.makeHtml(text2)
-                       throttle(setPreView, { args: html })
+                     textChanged: () => {
+                       throttle(textChangeHandle)
                      },
                    }} style={`border:1px solid #888888;width:500px;height:200px;`}
                    placeholderText="默认输入"/>
