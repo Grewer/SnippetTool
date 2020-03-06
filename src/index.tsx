dd508c3ddbc77922bd06cb001f242a96efdeb809
zhangyu <grewer@grewer.cn>
2020-03-06 23:55:27 +0800

    feat(style): 更新版本

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import App from "./app";
 
 process.title = "snippet";
-Renderer.render(<App />);
+Renderer.render(<App />, {
+  onInit: (reconciler: any) => {
+    if (process.env.NODE_ENV === "development") {
+      require("@nodegui/devtools").connectReactDevtools(reconciler);
+    }
+  }
+});
 
 if (module.hot) {
   module.hot.accept(["./app"], function() {
