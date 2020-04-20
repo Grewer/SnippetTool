a4e2b79c96d6061e48a4cb20e6f6327e05e5ef53
grewer <grewer@grewer.cn>
2020-04-21 00:19:26 +0800

    feat: file 样式

--- src/render/pages/FileLists.tsx
+++ src/render/pages/FileLists.tsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import styles from './FileLists.less'
 
+/**
+ * 类型分为文件和文件夹
+ * @constructor
+ */
 function FileLists() {
   const [list, setList]: [any[], React.Dispatch<React.SetStateAction<any[]>>] = useState([] as any[])
-  // useEffect(() => {
-  //   setList([...Array(50)].map((v, k) => k + 1))
-  // }, [])
+  const [active, setActive] = useState()
+
+  useEffect(() => {
+    setList([...Array(50)].map((v, k) => k + 1))
+  }, [])
+
+  const fileClickHandle = useCallback(index => {
+    setActive(index)
+  }, [])
 
   return (
     <div className={styles.fileList}>
@@ -13,14 +23,19 @@ function FileLists() {
       <div>search</div>
       <ul>
         {list.map((item, index) => {
-          return <li key={index.toString()}>{item}</li>
+          const className = active === index ? `${styles.item} ${styles.active}` : styles.item
+          return (
+            <li onClick={() => fileClickHandle(index)} className={className} key={index.toString()}>
+              {item}
+            </li>
+          )
         })}
       </ul>
     </div>
   )
 }
 
-function Header(props) {
+function Header() {
   return <div className={styles.header}>{/* 这里是拖曳区域 */}</div>
 }
 
