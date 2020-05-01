79ca7248fb6b41c8a8f1e41d6d7ad61dfa4f3a1c
grewer <grewer@grewer.cn>
2020-05-01 23:53:39 +0800

    feat: 添加图标

--- src/render/pages/FileLists.tsx
+++ src/render/pages/FileLists.tsx
@@ -37,8 +37,8 @@ function FileLists() {
 
   return (
     <div className={styles.fileList}>
-      <Header />
-      <button onClick={btnClick}>click</button>
+      {/* <Header /> */}
+      {/* <button onClick={btnClick}>click</button> */}
       <div>search</div>
       <ul>
         {list.map((item, index) => {
@@ -46,6 +46,7 @@ function FileLists() {
           return (
             // eslint-disable-next-line jsx-a11y/click-events-have-key-events
             <li onClick={() => fileClickHandle(index)} className={className} key={index.toString()}>
+              <i className="iconfont icon-jiantou" />
               {item}
             </li>
           )
