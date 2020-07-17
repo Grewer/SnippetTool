4dfee559b55f851206257de34141d1911a3cf0d6
zhangyu <grewer@grewer.cn>
2020-07-17 23:37:05 +0800

    feat: popover 样式基本完成

--- src/render/popover/FileMorePopover.tsx
+++ src/render/popover/FileMorePopover.tsx
@@ -5,7 +5,20 @@ const FileMorePopover = props => {
   return (
     <div className="popover-box">
       <i className="popover-arrow" />
-      显示的悬浮窗口
+      <ul className="popover-ul">
+        <li>
+          <i data-close="0" className="iconfont icon-shanchu" />
+          <span data-close="0">删除</span>
+        </li>
+        <li>
+          <i data-close="0" className="iconfont icon-yidongdaozu" />
+          <span data-close="0">移动</span>
+        </li>
+        <li>
+          <i data-close="0" className="iconfont icon-zhongmingming" />
+          <span data-close="0">重命名</span>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/render/popover/Popover.tsx
+++ src/render/popover/Popover.tsx
@@ -10,20 +10,27 @@ function BasePopover(Component, classKey: string, props?: any) {
   const show = ({ top = 0, left = 0 }) => {
     Dom.style.cssText = `
                           display:block;
-                          transform:translate(${left - 45}px, ${top + 25}px);
+                          transform:translate(${left - 55}px, ${top + 25}px);
                         `
   }
 
-  document.addEventListener('click', ev => {
-    const ele = ev.target as HTMLElement
-    if (ele.classList.contains('icon-more')) {
-      return
-    }
-    if (ele.className.includes('popover')) {
-      return
-    }
-    Dom.style.display = 'none'
-  })
+  document.addEventListener(
+    'click',
+    ev => {
+      const ele = ev.target as HTMLElement
+      if (ele.dataset.close === '0') {
+        return
+      }
+      if (ele.classList.contains('icon-more')) {
+        return
+      }
+      if (ele.className.includes('popover') || ele.parentElement?.className.includes('popover')) {
+        return
+      }
+      Dom.style.display = 'none'
+    },
+    false
+  )
 
   ReactDOM.render(<Component {...props} />, Dom)
 
