03a3039cd21fab50136a6654b8b7143e87aee892
zhangyu <grewer@grewer.cn>
2020-07-21 23:56:36 +0800

    feat: popover 暂时解决方案

--- src/render/pages/FileLists.tsx
+++ src/render/pages/FileLists.tsx
@@ -1,13 +1,11 @@
-import React, { createRef, useCallback, useContext, useEffect, useRef, useState } from 'react'
+import React, { FC, memo, useCallback, useContext, useState } from 'react'
 import BasePopover from '~/popover/Popover'
 import FileMorePopover from '~/popover/FileMorePopover'
 import styles from './FileLists.less'
-import fetch from '~/utils'
 import AddFileOrDir from '~/modals/AddFileOrDir'
 import ConfigContext from '~/context/ConfigContext'
 import IFileType from '~/enum/FileType'
 import { IFileListItem } from '~/definition/Main'
-import useMount from '~/hooks/useMount'
 
 /**
  * 类型分为文件和文件夹
@@ -15,9 +13,68 @@ import useMount from '~/hooks/useMount'
  */
 
 function FileLists() {
+  console.log('%c render FileLists', 'background:yellow;')
+
+  // const btnClick = () => {
+  //   fetch.config.withCredentials = true
+  //   fetch.config.headers = {
+  //     Authorization: 'token ',
+  //   }
+  //   fetch.get(`https://api.github.com/repos/Grewer/dataSave/contents/back.jpg`)
+  //
+  //   fetch.put(`https://api.github.com/repos/Grewer/dataSave/contents/aa/test.txt`, {
+  //     message: '使用 api 测试提交文件',
+  //     content: btoa('test'),
+  //     committer: {
+  //       name: 'grewer',
+  //       email: 'grewer@grewer.cn',
+  //     },
+  //   })
+  // }
+
+  const addFileOrDir = useCallback(() => {
+    AddFileOrDir().open()
+  }, [])
+
+  const [popover, setPopover] = useState({ position: '', item: {}, setPopover: value => {} })
+
+  const popoverClick = useCallback((ev, item) => {
+    ev.stopPropagation()
+    const distance = ev.target.getBoundingClientRect()
+
+    setPopover(prevState => {
+      if (prevState.item === item) {
+        return prevState
+      }
+      return {
+        position: `${distance.left - 55}px, ${distance.top + 25}px`,
+        item,
+        setPopover,
+      }
+    })
+  }, [])
+
+  return (
+    <div className={styles.fileList}>
+      <Header />
+      {/* <button onClick={btnClick}>click</button> */}
+      <div>全局的搜索按钮34</div>
+      <div className={styles.title}>
+        文件夹列表
+        <i onClick={() => addFileOrDir()} className="iconfont icon-jia" />
+      </div>
+      <FileListView popoverClick={popoverClick} />
+      <FileMorePopover popover={popover} />
+    </div>
+  )
+}
+
+const FileListView: FC<{ popoverClick: (ev, item) => void }> = memo(props => {
+  console.log('%c render FileListView', 'background:yellow;')
+
   const { fileList, current, setCurrent } = useContext(ConfigContext)
 
-  console.log('render FileLists', current)
+  const currentId = current.id
 
   const fileClickHandle = useCallback(
     (item: IFileListItem) => {
@@ -32,71 +89,27 @@ function FileLists() {
     [setCurrent, current]
   )
 
-  const btnClick = () => {
-    fetch.config.withCredentials = true
-    fetch.config.headers = {
-      Authorization: 'token ',
-    }
-    fetch.get(`https://api.github.com/repos/Grewer/dataSave/contents/back.jpg`)
-
-    fetch.put(`https://api.github.com/repos/Grewer/dataSave/contents/aa/test.txt`, {
-      message: '使用 api 测试提交文件',
-      content: btoa('test'),
-      committer: {
-        name: 'grewer',
-        email: 'grewer@grewer.cn',
-      },
-    })
-  }
-
   const iconClickHandle = useCallback(index => {
     // AddFileOrDir().open()
   }, [])
 
-  const addFileOrDir = useCallback(() => {
-    AddFileOrDir().open()
-  }, [])
-
-  const currentId = current.id
-
-  const popoverClick = (ev, item) => {
-    ev.stopPropagation()
-    const show = BasePopover(FileMorePopover, 'popover-fileMore', { item })
-    const distance = ev.target.getBoundingClientRect()
-    show({
-      top: distance.top,
-      left: distance.left,
-    })
-  }
-
-  // console.log('render popoverClickRef', popoverClickRef)
-
   return (
-    <div className={styles.fileList}>
-      <Header />
-      {/* <button onClick={btnClick}>click</button> */}
-      <div>全局的搜索按钮34</div>
-      <div className={styles.title}>
-        文件夹列表
-        <i onClick={() => addFileOrDir()} className="iconfont icon-jia" />
-      </div>
-      <ul>
-        {fileList.map((item, index) => {
-          const { id } = item
-          const className = currentId === id ? `${styles.item} ${styles.active}` : styles.item
-          return (
-            <li onClick={() => fileClickHandle(item)} className={className} key={id}>
-              <span className={styles.fileName}>
-                {item.fileType === IFileType.folder && <i onClick={() => iconClickHandle(index)} className="iconfont icon-jiantou" />} {item.fileName}
-              </span>
-              <Control moreClick={ev => popoverClick(ev, item)} fileType={item.fileType} />
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+    <ul>
+      {fileList.map((item, index) => {
+        const { id } = item
+        const className = currentId === id ? `${styles.item} ${styles.active}` : styles.item
+        return (
+          <li onClick={() => fileClickHandle(item)} className={className} key={id}>
+            <span className={styles.fileName}>
+              {item.fileType === IFileType.folder && <i onClick={() => iconClickHandle(index)} className="iconfont icon-jiantou" />} {item.fileName}
+            </span>
+            <Control moreClick={ev => props.popoverClick(ev, item)} fileType={item.fileType} />
+          </li>
+        )
+      })}
+    </ul>
   )
-}
+})
 
 function Control(props: { fileType: IFileType; moreClick: (event: React.MouseEvent) => void }) {
   const Add = () => {
--- src/render/popover/FileMorePopover.tsx
+++ src/render/popover/FileMorePopover.tsx
@@ -1,36 +1,77 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useCallback, useContext, useEffect, useRef } from 'react'
 import './popover.global.less'
+import ReactDOM from 'react-dom'
 import ConfigContext from '~/context/ConfigContext'
 
-const FileMorePopover = props => {
-  const { fileList, current, setCurrent } = useContext(ConfigContext)
+const { body } = document
 
-  console.log('render FileMorePopover', current, props)
+const FileMorePopover = props => {
+  console.log('%c render FileMorePopover', 'background:yellow;', props)
+
+  const { position, item, setPopover } = props.popover
+
+  const containerRef: React.MutableRefObject<any> = useRef()
+
   const deleteHandler = useCallback(() => {
-    console.log(current, props)
-  }, [current, props])
+    console.log(props)
+  }, [props])
   const moveHandler = useCallback(() => {}, [])
   const renameHandler = useCallback(() => {}, [])
 
-  return (
-    <div className="popover-box">
-      <i className="popover-arrow" />
-      <ul className="popover-ul">
-        <li onClick={deleteHandler}>
-          <i data-close="0" className="iconfont icon-shanchu" />
-          <span data-close="0">删除</span>
-        </li>
-        <li onClick={moveHandler}>
-          <i data-close="0" className="iconfont icon-yidongdaozu" />
-          <span data-close="0">移动</span>
-        </li>
-        <li onClick={renameHandler}>
-          <i data-close="0" className="iconfont icon-zhongmingming" />
-          <span data-close="0">重命名</span>
-        </li>
-      </ul>
-    </div>
+  const triggerHandler = useCallback(
+    e => {
+      const ele = e.target
+      if (containerRef.current?.contains(ele) || ele.classList.contains('icon-more')) {
+        console.log('click inside')
+      } else {
+        console.log('click outside')
+        setPopover({
+          position: ``,
+          item: {},
+          setPopover,
+        })
+      }
+    },
+    [setPopover]
   )
+
+  useEffect(() => {
+    document.addEventListener('click', triggerHandler)
+    return () => {
+      document.removeEventListener('click', triggerHandler)
+    }
+  }, [triggerHandler])
+
+  return position
+    ? ReactDOM.createPortal(
+        <div
+          ref={containerRef}
+          className="popover-fileMore"
+          style={{
+            transform: `translate(${position})`,
+          }}
+        >
+          <div className="popover-box">
+            <i className="popover-arrow" />
+            <ul className="popover-ul">
+              <li onClick={deleteHandler}>
+                <i data-close="0" className="iconfont icon-shanchu" />
+                <span data-close="0">删除</span>
+              </li>
+              <li onClick={moveHandler}>
+                <i data-close="0" className="iconfont icon-yidongdaozu" />
+                <span data-close="0">移动</span>
+              </li>
+              <li onClick={renameHandler}>
+                <i data-close="0" className="iconfont icon-zhongmingming" />
+                <span data-close="0">重命名</span>
+              </li>
+            </ul>
+          </div>
+        </div>,
+        body
+      )
+    : null
 }
 
 export default FileMorePopover
